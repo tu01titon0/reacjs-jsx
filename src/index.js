81fd38cd2aad13c18719180bf7d621e2f4ddb127
tu <nguyenmanhtu09042000@gmail.com>
2023-07-05 08:56:02 +0700

    set-intervel

--- src/index.js
+++ src/index.js
@@ -5,26 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const fruits = [
-    "Apple",
-    "Banana",
-    "Orange",
-    "Apricot",
-    "Black rowan",
-    "Cranberry"
-];
 
-root.render(
-    // React.createElement("h1", { style: { textAlign: "center" } }, name)
-    <div>
-    <h1>List of fruits</h1>
-        <ul>
-            { fruits.map((item) => (
-                <li>{ item }</li>
-            )) }
-        </ul>
-    </div>
-);
+const tick = () => {
+    root.render(
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+};
+
+setInterval(tick, 10000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
