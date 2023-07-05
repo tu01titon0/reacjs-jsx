a17b2990a9f51752a04fa6421f52a721fbe0dbcd
tu <nguyenmanhtu09042000@gmail.com>
2023-07-05 08:48:59 +0700

    jsx-list

--- src/index.js
+++ src/index.js
@@ -5,10 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const name = "Your name";
+const fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Apricot",
+    "Black rowan",
+    "Cranberry"
+];
 
 root.render(
-    React.createElement("h1", { style: { textAlign: "center" } }, name)
+    // React.createElement("h1", { style: { textAlign: "center" } }, name)
+    <div>
+    <h1>List of fruits</h1>
+        <ul>
+            { fruits.map((item) => (
+                <li>{ item }</li>
+            )) }
+        </ul>
+    </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
